fb3fe81951a80663c835d12a9c65101bc20b59f3
Abhishek Vishal <abhishekvishal842002@gmail.com>
2022-05-06 22:55:35 +0530

    first commit

--- array_Q5.cpp
+++ array_Q5.cpp
@@ -1,6 +1,6 @@
 // given an array[] of size N. 
 // the task is to find the first repeating element in the array of integers,i.e.,
-// an element that occurs more than once and whose index of first occurence is smallest.
+// an element that  occurs more than once and whose index of first occurence is smallest.
 
 #include<iostream>
 using namespace std;
